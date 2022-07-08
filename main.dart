e3c3d53f05b86722590c7bc1e4fa920cba3b3739
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-08 06:29:27 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,34 @@
+void main() {
+  
+/*	int a = 0;
+  while(a < 3){
+    print("hello");
+    a++;
+  }*/
+
+  /*
+  int a = 4;
+  while(a>=0){
+    print("hello");
+    --a;
+  }
+*/
+
+  /*
+  int i = 0;
+
+  do{
+    print(i);
+    i++;
+  }while(i<10);
+*/
+  
+
+  /* karra jadvali 
+  for(int x=1;x<=10;x++){
+    for(int y=1;y<=10;y++){
+      print("$x * $y = ${x*y}");
+    }
+  }
+  */
+}
